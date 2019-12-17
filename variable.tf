195efffb266956b233fff7445135d438d24d784c
imlekavita <56871458+imlekavita@users.noreply.github.com>
2019-12-17 13:00:02 +0530

    update

---
+++ variable.tf
@@ -0,0 +1,17 @@
+#types of varible
+#string
+#number int
+#bool boolean
+
+variable a-string{
+type = string
+
+}
+variable "true or false"{
+type = bool
+
+}
+variable "this_is_a_number"{
+type = number
+
+}
